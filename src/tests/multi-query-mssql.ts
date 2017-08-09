3ba869a04835b41c7625af19be2332e7cbf43d31
Joe France <joefrance@numerigy.com>
2017-08-08 21:45:25 -0400

    Added callQueriesSync()

--- src/tests/multi-query-mssql.ts
+++ src/tests/multi-query-mssql.ts
@@ -2,27 +2,45 @@ const fs = require('fs');
 
 import { MSSQLHelper } from "../lib/database/mssql/execute-query-helper";
 
-declare var __dirname;
-var importString: string = '';
 var args = process.argv.slice(2);
-//console.dir(args);
 
 var argv = require('minimist')(process.argv.slice(2));
-//console.dir(argv.schema);
-//console.dir(argv.table);
 if  (
         argv.configpath == undefined
     ) {
+
+        // See contents of:
+        // mssql_example_connection_config_json
+        // for example connection information
+        // It may be possible to use this with
+        // windows with the addition of the
+        // mssql/msnodesqlv8 driver
+        // and setting trustedConnection: true
+
         console.log("Usage: node multi-query-mssql --configpath connection.config.json")
 } else {
     var config = JSON.parse(fs.readFileSync(argv.configpath, 'utf8'));
-    console.log(config);
 
     var qh: MSSQLHelper.ExecuteQueryHelper = new MSSQLHelper.ExecuteQueryHelper();
 
-    qh.connectToServer(config).then(result => {
-        console.log(result);
+    callQueriesSync(config);
+
+}
+
+// This function will run syncronously
+// inside the async braces
+// This simplifies "cascading" queries
+async function callQueriesSync(config: any): Promise<void> {
+
+    let result = await qh.connectToServer(config);
+    let qr = await qh.executeQueryAndWait("select [name] from sysobjects where xtype = 'U' and [name] not like '%_shadow'");
+    for(let ix=0; ix < qr.result.recordset.length; ix++) {
+        let sql = `select count(1) NumRows from [${qr.result.recordset[ix].name}]`;
+
+        let qr2 = await qh.executeQueryAndWait(sql);
+
+        console.log(`${qr.result.recordset[ix].name}: ${qr2.result.recordset[0].NumRows}`);
+    }
         
-        qh.close();
-    });
+    qh.close();
 }
